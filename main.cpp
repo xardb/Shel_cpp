edbcea17ff0ff3f886370bb2822587bb4c83989b
xardb <vlad96mir@bk.ru>
2025-09-27 09:54:31 +0300

    1. Печатает введённую строку и выходит

---
+++ main.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    system("chcp 65001");
+    string input;
+    cout << "мой шелл :)" << endl;
+    cout << "₽ ";
+    getline(cin, input);
+
+
+}
